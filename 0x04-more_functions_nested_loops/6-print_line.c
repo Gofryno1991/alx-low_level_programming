56a182c79d1b6a8b73dfefdb0f8dd1bc29dab76c
Godfrey <godfreyibhgbosoria@gmail.com>
2022-07-11 11:44:07 -0700

    tryin

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - check for a digit
+ * @n : number of _to be printed
+ * *Return:void
+ */
+
+void print_line(int n);
+int main(void)
+{
+
+	int l = 20;
+	print_line(l);
+}
+void print_line(int l)
+{
+	int i = 0;
+	while (i <=2)
+	{
+		printf("_");
+	}
+}
